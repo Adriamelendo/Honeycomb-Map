a0691b91382a19d1d21e5e88ef53cfd5fb88497c
Adrià Melendo <adriamelendo@gmail.com>
2020-07-15 23:02:45 +0200

    nice layout, still small error in height of panels and scroll, but ok for demo

--- frontend/src/app/map/hex-contents/hex-contents.component.ts
+++ frontend/src/app/map/hex-contents/hex-contents.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnChanges, Input } from '@angular/core';
 import { HCMapRegion, HexContents } from '../../services/hcmap-data.service';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-hex-contents',
@@ -14,17 +15,25 @@ export class HexContentsComponent implements OnChanges {
   provinces: HCMapRegion[] = [];
   showNoResult: boolean = true;
 
+  layout1:boolean = true;
+  layout2:boolean = false;
+  layout3:boolean = false;
+  layout4:boolean = false;
+
+
 
   constructor() { }
 
   ngOnChanges() {
     console.log('HexContents');
     this.showNoResult = true;
+    let numPanels = 0;
     if (this.hexContents) {
       console.log('regions:', this.hexContents.regions);
       console.log('resources:', this.hexContents.resources);
       if (this.hexContents.resources.length !== 0) {
         this.showNoResult = false;
+        numPanels++;
       }
 
       this.towns = [];
@@ -35,13 +44,42 @@ export class HexContentsComponent implements OnChanges {
           if (reg.type === 'town') {
             this.towns.push(reg);
             this.showNoResult = false;
+            numPanels++;
           }
           if (reg.type === 'province') {
             this.provinces.push(reg);
             this.showNoResult = false;
+            numPanels++;
           }
         }
       });
+      console.log('We must draw '+numPanels+' panels');
+      this.layout1=false;
+      this.layout2=false;
+      this.layout3=false;
+      this.layout4=false;
+      switch(numPanels) { 
+        case 3: { 
+           this.layout1=true;
+           console.log('layout1');
+           break; 
+        } 
+        case 2: { 
+          if (this.hexContents.resources.length == 0) {
+            this.layout2=true;
+            console.log('layout2');
+          } else {
+            this.layout3=true;
+            console.log('layout3');
+          }
+           break; 
+        } 
+        case 1: { 
+          this.layout4=true; 
+          console.log('layout4');
+          break; 
+       }
+     } 
 
     } else {
       console.log('empty');
