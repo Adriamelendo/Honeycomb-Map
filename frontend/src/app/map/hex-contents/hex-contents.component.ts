a1585e186a126f94b9e98d472028024661cc736f
Adrià Melendo <adriamelendo@gmail.com>
2020-07-16 17:05:21 +0200

    detect windows size and insert slider

--- frontend/src/app/map/hex-contents/hex-contents.component.ts
+++ frontend/src/app/map/hex-contents/hex-contents.component.ts
@@ -1,21 +1,48 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import { Component, OnChanges, OnInit, Input, HostListener } from '@angular/core';
 import { HexContents, HCMapRegion, HCMapResource } from '../../services/hcmap-data.service';
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Platform } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-hex-contents',
   templateUrl: './hex-contents.component.html',
   styleUrls: ['./hex-contents.component.scss'],
 })
-export class HexContentsComponent implements OnChanges {
+export class HexContentsComponent implements OnChanges, OnInit {
 
   @Input() hexContents: HexContents;
-
+  
   public towns: HCMapRegion[];
   public provinces: HCMapRegion[];
   public resources: HCMapResource[];
 
-  constructor() { }
+  biggerThan680: boolean = false;
+
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
+
+  constructor(public platform: Platform) {}
+  
+  ngOnInit() {
+    console.log(this.platform.width());
+    if (this.platform.width() > 680) {
+      this.biggerThan680 = true;
+    } else {
+      this.biggerThan680 = false;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])onResize(event) {
+    console.log(this.platform.width());
+    if (this.platform.width() > 680) {
+      this.biggerThan680 = true;
+    } else {
+      this.biggerThan680 = false;
+    }
+  }
 
   ngOnChanges() {
     console.log('HexContents');
@@ -23,11 +50,11 @@ export class HexContentsComponent implements OnChanges {
     if (this.hexContents) {
       this.towns = this.hexContents.regions.filter(
         (region) => (region.type === 'town' &&
-                     region.resources.length > 0)
+          region.resources.length > 0)
       );
       this.provinces = this.hexContents.regions.filter(
         (region) => (region.type === 'province' &&
-                     region.resources.length > 0)
+          region.resources.length > 0)
       );
       this.resources = this.hexContents.resources;
     } else {
